ec1311e9b835f90f8113be48cdeb341fdf8b3c39
Stefan Graupner <stefan.graupner@gmail.com>
2016-01-06 23:28:01 +0100

    More or less finished.

--- scrapers/karreraklub/karreraklub.py
+++ scrapers/karreraklub/karreraklub.py
@@ -2,14 +2,15 @@ from bs4 import BeautifulSoup
 import urllib
 import json
 import time
-import datetime
+from datetime import datetime
+import hashlib
 
 url = "http://www.karreraklub.de/karrera/termine/liste"
 page = urllib.urlopen(url).read()
 
 soup = BeautifulSoup(page)
 
-events = soup.findAll('div', class_='termin_info')
+events = soup.findAll('div', class_='termin_content')
 
 result = []
 
@@ -18,27 +19,20 @@ for event in events:
     time_text = event.find('span', class_='termin_info_uhrzeit').text
 
     ts = time.strptime(date_text[4:] + ' ' + time_text, '%d.%m.%Y %H:%M')  
-    starting_time = datetime.datetime.fromtimestamp(time.mktime(ts)).isoformat()
+    starting_time = datetime.fromtimestamp(time.mktime(ts)).isoformat()
 
     location = event.find('span', class_='termin_info_ort').text
 
     title = event.find('span', class_='termin_info_name').text
-
-    description = ''
-
-    full_name = event.find_all('span', class_='termin_info_name')
-    for part in full_name:
-        description += part.text + '\n'
-
-    bands = event.find_all('span', class_='termin_info_live')
-    for band in bands:
-        description += band.text #+ ' ' + band.next_element.text + '\n'
+    description = event.find('div', class_='termin_live_info_text').text
 
     result.append({ 
         'starting_time': starting_time, 
-        'human_location_name': location, 
+        'location': { 'human_name': location }, 
         'title': title,
-        'description': description
+        'description': description,
+        'hash': hashlib.sha1(starting_time + location).hexdigest(),
+        'url': url # TODO: This may need a little refinement
     })
 
 print(json.dumps(result, indent=4))
